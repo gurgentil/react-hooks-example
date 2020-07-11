9d38af0bd08bb2c1ac3d10e66e904a2ac7354282
Gustavo Gentil <gustavorgentil@outlook.com>
2020-07-11 02:51:52 -0300

    feat: create button to add repositories

--- src/App.js
+++ src/App.js
@@ -7,10 +7,24 @@ function App() {
     { id: 3, name: 'repo-3' },
   ]);
 
+  function addRepository() {
+    const id = Math.random();
+
+    setRepositories([
+      ...repositories,
+      { id, name: `repo-${ id }` },
+    ]);
+  }
+
   return (
-    <ul>
-      { repositories.map(repository => <li key={ repository.id }>{ repository.name }</li>) }
-    </ul>
+    <>
+      <ul>
+        { repositories.map(repository => <li key={ repository.id }>{ repository.name }</li>) }
+      </ul>
+      <button onClick={ addRepository }>
+        Adicionar Repositório
+      </button>
+    </>
   );
 }
 
